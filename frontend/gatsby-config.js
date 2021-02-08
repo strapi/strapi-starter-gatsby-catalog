0ebdcbd6c0863e408c035531c3604916c0905391
Mark Kaylor <mark.kaylor@strapi.io>
2021-02-08 11:03:05 +0100

    Change favicon

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/strapi-icon.svg`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
