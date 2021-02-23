402e646cd585508efcc0dc39011349d320a7b7d4
Mark Kaylor <mark.kaylor@strapi.io>
2021-02-23 13:56:50 +0100

    Add api url env variable

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.API_URL || `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`product`, `category`],
         singleTypes: [`global`],
