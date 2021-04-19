60673790a3ca591357eade711b090b3ccd8f3a98
Mark Kaylor <mark.kaylor@strapi.io>
2021-04-19 10:09:28 +0200

    upgrade to gatsby v3

--- starter/gatsby-node.js
+++ starter/gatsby-node.js
@@ -1,11 +1,20 @@
 const path = require("path")
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ actions, plugins, stage }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
         "~": path.resolve(__dirname, "src"),
+        path: require.resolve("path-browserify"),
+      },
+      fallback: {
+        fs: false,
       },
     },
   })
+  if (stage === "build-javascript" || stage === "develop") {
+    actions.setWebpackConfig({
+      plugins: [plugins.provide({ process: "process/browser" })],
+    })
+  }
 }
